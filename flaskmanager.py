b1e41cf5e402eb2fd069255002de8b77e11b3c37
Nadeem Mir <76222815+Nadeemmir873216@users.noreply.github.com>
2025-02-22 08:16:35 +0000

    Start of flask

--- flaskmanager.py
+++ flaskmanager.py
@@ -5,7 +5,7 @@ import os
 html_save_path = 'FlaskManagerCheck'
 csv_save_path = 'FlaskManagerCheck'
 category_id = 52
-pages=5
+pages=1
 
 
 if not os.path.exists(html_save_path):
@@ -13,6 +13,22 @@ if not os.path.exists(html_save_path):
 if not os.path.exists(csv_save_path):
     os.mkdir(csv_save_path)
 
-scrape_html(category_id, html_save_path, pages=pages)
+# scrape_html(category_id, html_save_path, pages=pages)
 
-html_csv(html_save_path, category_id, csv_save_path)
+# html_csv(html_save_path, category_id, csv_save_path)
+
+
+
+### Complete Flask Code starts here
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+if __name__ == '__main__': 
+    app.run(debug=True, port=5000)
